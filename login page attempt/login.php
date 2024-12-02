5d0536225b30c5f123af77d884c80af7b9e10384
Aaron Szalai <karcsipami12@gmail.com>
2024-12-02 13:22:24 +0100

    Some changes

--- login page attempt/login.php
+++ login page attempt/login.php
@@ -10,18 +10,20 @@
 <body>
     <div class="wrapper">
         <h1>Bejelentkezés</h1>
-        <form id="form">            
+        <form id="form" action="login.php" method="POST">            
             <div class="input-box">
-                <input type="email" name="email" id="email-input" placeholder="Email" required>
+                <label for="email">E-mail:</label>
+                <input type="email" name="email" id="email-input" required="" />
             </div>
             <div class="input-box">
-                <input type="password" name="password" id="password-input" placeholder="Jelszó" required>
+                <label for="password">Jelszó:</label>
+                <input type="password" name="password" id="password-input" required="" />
             </div>
             <div class="remember-forget">
                 <label ><input type="checkbox">Jelszó megjegyzése</label> 
                 <a href="#">Elfelejtette jelszavát?</a>
             </div>
-            <button type="submit" class="button">Belépés</button>
+            <input name="login" id="loginButton" type="submit" class="button" value="Belépés" />
             <div class="register-link">
                 <p>Nincs még fiókod? <a href="register.html">Regizstrálj!</a> </p>
             </div>
