d5a2e0ee72ec7fbce871c482b2e4828bb38da4a3
Aaron Szalai <karcsipami12@gmail.com>
2024-12-11 11:00:17 +0100

    connect

--- login page attempt/connect.php
+++ login page attempt/connect.php
@@ -10,4 +10,4 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 
 //ha nem jött létre a kapcsolat
 if (!$conn) {
-    die("A kapcsolat nem jött létre: ". mysqli_connect_error());
+    die("A kapcsolat nem jött létre: ".mysqli_connect_error());
