7f0e17c37a5fc8d52930c3391e7cb56b6b4ad50f
aron-77 <radolfiaron05@gmail.com>
2025-04-08 12:11:56 +0200

    www

--- scheduler/save_completed_task.php
+++ scheduler/save_completed_task.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "password";
-$dbname = "scheduler_db";
+$dbname = "task_manager";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
